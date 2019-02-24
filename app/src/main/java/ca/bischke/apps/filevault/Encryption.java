b1a6bc4f0ce0b71a0427139ed99398e431af5f93
Dustin Bischke <dustin@bischke.ca>
2019-02-24 22:38:52 +0000

    Split Encryption and KeyStore classes

---
+++ app/src/main/java/ca/bischke/apps/filevault/Encryption.java
@@ -0,0 +1,129 @@
+package ca.bischke.apps.filevault;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+
+public class Encryption
+{
+    private Context context;
+    private KeyStore keyStore;
+    private final String keyAlgorithm = "PBKDF2WithHmacSHA1";
+    private final String cipherAlgorithm = "AES/CBC/PKCS5Padding";
+    private IvParameterSpec iv;
+    private byte[] salt;
+
+    public Encryption(Context context)
+    {
+        this.context = context;
+        this.keyStore = new KeyStore(context);
+    }
+
+    private byte[] getRandomByteArray(int size)
+    {
+        SecureRandom random = new SecureRandom();
+        byte[] randomByteArray = new byte[size];
+        random.nextBytes(randomByteArray);
+
+        return randomByteArray;
+    }
+
+    private IvParameterSpec getIV()
+    {
+        if (iv == null)
+        {
+            String key = context.getString(R.string.preference_iv);
+            byte[] ivBytes = keyStore.getBytes(key);
+
+            if (ivBytes == null)
+            {
+                ivBytes = getRandomByteArray(16);
+                keyStore.setBytes(key, ivBytes);
+            }
+
+            iv = new IvParameterSpec(ivBytes);
+        }
+
+        return iv;
+    }
+
+    private byte[] getSalt()
+    {
+        if (salt == null)
+        {
+            String key = context.getString(R.string.preference_salt);
+            byte[] saltBytes = keyStore.getBytes(key);
+
+            if (saltBytes == null)
+            {
+                saltBytes = getRandomByteArray(16);
+                keyStore.setBytes(key, saltBytes);
+            }
+
+            salt = saltBytes;
+        }
+
+        return salt;
+    }
+
+    private SecretKey getSecretKey(String password, byte[] salt)
+            throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        int iterations = 10000;
+        int outputLength = 128;
+
+        char[] passwordArray = password.toCharArray();
+        KeySpec keySpec = new PBEKeySpec(passwordArray, salt, iterations, outputLength);
+
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(keyAlgorithm);
+        SecretKey secretKey = secretKeyFactory.generateSecret(keySpec);
+
+        return secretKey;
+    }
+
+    private void doCrypto(int cipherMode, String password, File inputFile, File outputFile)
+            throws GeneralSecurityException, IOException
+    {
+        SecretKey secretKey = getSecretKey(password, getSalt());
+        Cipher cipher = Cipher.getInstance(cipherAlgorithm);
+        cipher.init(cipherMode, secretKey, getIV());
+
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        byte[] inputBytes = new byte[(int) inputFile.length()];
+        inputStream.read(inputBytes);
+
+        byte[] outputBytes = cipher.doFinal(inputBytes);
+
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        outputStream.write(outputBytes);
+
+        inputStream.close();
+        outputStream.close();
+    }
+
+    public void encrypt(String password, File inputFile, File outputFile)
+            throws GeneralSecurityException, IOException
+    {
+        doCrypto(Cipher.ENCRYPT_MODE, password, inputFile, outputFile);
+    }
+
+    public void decrypt(String password, File inputFile, File outputFile)
+            throws GeneralSecurityException, IOException
+    {
+        doCrypto(Cipher.DECRYPT_MODE, password, inputFile, outputFile);
+    }
+}
---
+++ app/src/main/java/ca/bischke/apps/filevault/KeyStore.java
@@ -0,0 +1,58 @@
+package ca.bischke.apps.filevault;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class KeyStore
+{
+    private final String file;
+    private SharedPreferences sharedPreferences;
+
+    public KeyStore(Context context)
+    {
+        file = context.getString(R.string.app_name);
+        sharedPreferences = context.getSharedPreferences(file, Context.MODE_PRIVATE);
+    }
+
+    public boolean exists(String key)
+    {
+        return sharedPreferences.contains(key);
+    }
+
+    public byte[] getBytes(String key)
+    {
+        if (exists(key))
+        {
+            String valueString = sharedPreferences.getString(key, null);
+            return Convert.getByteArrayFromString(valueString);
+        }
+
+        return null;
+    }
+
+    public void setBytes(String key, byte[] valueBytes)
+    {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        String value = Convert.getStringFromByteArray(valueBytes);
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public void remove(String key)
+    {
+        if (exists(key))
+        {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.remove(key);
+            editor.apply();
+        }
+    }
+
+    public void clear()
+    {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+    }
+}
