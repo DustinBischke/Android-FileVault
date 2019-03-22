ef4740b36110c30c57cd6c3f342715dd08b3620b
Dustin Bischke <dustin@bischke.ca>
2019-03-22 18:47:14 +0000

    Added Login Activity

--- app/src/main/java/ca/bischke/apps/filevault/LoginActivity.java
+++ app/src/main/java/ca/bischke/apps/filevault/LoginActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,13 +14,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class RegisterActivity extends AppCompatActivity
+public class LoginActivity extends AppCompatActivity
 {
     private final String TAG = "FileVault";
     private FirebaseAuth firebaseAuth;
     private EditText editTextEmail;
     private EditText editTextPassword;
-    private EditText editTextPassword2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -39,35 +37,26 @@ public class RegisterActivity extends AppCompatActivity
         }
 
         // Sets Activity Layout
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
 
         editTextEmail = findViewById(R.id.edittext_email);
         editTextPassword = findViewById(R.id.edittext_password);
-        editTextPassword2 = findViewById(R.id.edittext_confirm_password);
 
         editTextEmail.addTextChangedListener(new EditTextWatcher(editTextEmail));
         editTextPassword.addTextChangedListener(new EditTextWatcher(editTextPassword));
-        editTextPassword2.addTextChangedListener(new EditTextWatcher(editTextPassword2));
 
         firebaseAuth = FirebaseAuth.getInstance();
     }
 
     // TODO Improve error display layout
-    public void buttonRegister(View view)
+    public void buttonLogin(View view)
     {
         String email = editTextEmail.getText().toString();
         String password = editTextPassword.getText().toString();
-        String password2 = editTextPassword2.getText().toString();
 
         if (TextUtils.isEmpty(email))
         {
             editTextEmail.setError("Email cannot be blank");
-            return;
-        }
-
-        if (!isValidEmail(email))
-        {
-            editTextEmail.setError("Email address is not valid");
             return;
         }
 
@@ -77,43 +66,19 @@ public class RegisterActivity extends AppCompatActivity
             return;
         }
 
-        // TODO Password requirements
-        if (password.length() < 6)
-        {
-            editTextPassword.setError("Password must be greater than 6 characters");
-            return;
-        }
-
-        if (TextUtils.isEmpty(password2))
-        {
-            editTextPassword2.setError("Confirm password cannot be blank");
-            return;
-        }
-
-        if (!password.equals(password2))
-        {
-            editTextPassword2.setError("Passwords do not match");
-            return;
-        }
-
-        createAccount(email, password);
+        login(email, password);
     }
 
-    public void buttonLogin(View view)
+    public void buttonRegister(View view)
     {
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
         finish();
     }
 
-    private boolean isValidEmail(String email)
+    private void login(String email, String password)
     {
-        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
-    }
-
-    private void createAccount(String email, String password)
-    {
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
+        firebaseAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>()
                 {
                     @Override
@@ -122,11 +87,11 @@ public class RegisterActivity extends AppCompatActivity
                         // TODO Switch to Activity / Display error
                         if (task.isSuccessful())
                         {
-                            Log.d(TAG, "Registration successful :)");
+                            Log.d(TAG, "Login successful :)");
                         }
                         else
                         {
-                            Log.d(TAG, "Registration failed :(");
+                            Log.d(TAG, "Login failed :(");
                         }
                     }
                 });
